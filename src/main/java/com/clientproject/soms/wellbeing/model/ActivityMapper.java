8eda8887f798636211bbdfa799621112e3447a81
Junwei Li <lij185@cardiff.ac.uk>
2020-12-16 15:45:10 +0800

    Create ActivityMapper.java

---
+++ src/main/java/com/clientproject/soms/wellbeing/model/ActivityMapper.java
@@ -0,0 +1,24 @@
+package com.clientproject.soms.wellbeing.model;
+
+
+import com.clientproject.soms.wellbeing.DTO.ActivityDTO;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ActivityMapper implements RowMapper {
+
+    //`ACTIVITY_ID`, `ACTIVITY_NAME`, `SERV_PROV_ID`, `ACTIVITY_DATE`, `DESCRIPTION`, `LOCATION`
+
+    @Override
+    public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new ActivityDTO(rs.getInt("ACTIVITY_ID"),
+                rs.getString("ACTIVITY_NAME"),
+                rs.getInt("SERV_PROV_ID"),
+                rs.getDate("ACTIVITY_DATE"),
+                rs.getString("DESCRIPTION"),
+                rs.getString("LOCATION"),
+                "******");
+    }
+}
