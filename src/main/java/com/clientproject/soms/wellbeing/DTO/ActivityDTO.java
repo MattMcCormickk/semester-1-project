dc1c9eb5db0b5639015aa87a38d44f3e36e47d5b
Junwei Li <lij185@cardiff.ac.uk>
2020-12-16 15:30:21 +0800

    Update ActivityDTO depend on the table we create.

--- src/main/java/com/clientproject/soms/wellbeing/DTO/ActivityDTO.java
+++ src/main/java/com/clientproject/soms/wellbeing/DTO/ActivityDTO.java
@@ -1,54 +1,69 @@
 package com.clientproject.soms.wellbeing.DTO;
 
+import java.util.Date;
+
 public class ActivityDTO {
-    private String activityName;
     private String activityID;
+    private String activityName;
+    private Date date;
     private String Description;
     private String Location;
     private String Keywords;
-    private String userID;
-    private String metric1;
-    private String metric2;
-    private String metric3;
 
-    public ActivityDTO(String activityName, String activityID, String Description, String Location, String Keywords,
-                       String userID, String metric1, String metric2, String metric3){
-        this.activityName = activityName;
+    public ActivityDTO(String activityID, String activityName, Date date, String description, String location, String keywords) {
         this.activityID = activityID;
-        this.Description = Description;
-        this.Location = Location;
-        this.Keywords = Keywords;
-        this.userID = userID;
-        this.metric1 = metric1;
-        this.metric2 = metric2;
-        this.metric3 = metric3;
-    }
-
-    public String getActivityName() {
-        return activityName;
+        this.activityName = activityName;
+        this.date = date;
+        Description = description;
+        Location = location;
+        Keywords = keywords;
     }
 
     public String getActivityID() {
         return activityID;
     }
 
+    public void setActivityID(String activityID) {
+        this.activityID = activityID;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
     public String getDescription() {
         return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
     }
 
     public String getLocation() {
         return Location;
     }
 
+    public void setLocation(String location) {
+        Location = location;
+    }
+
     public String getKeywords() {
         return Keywords;
     }
 
-    public String getUserID(){ return userID;}
-
-    public String getMetric1() {return metric1;}
-
-    public String getMetric2() {return metric2;}
-
-    public String getMetric3() {return metric3;}
+    public void setKeywords(String keywords) {
+        Keywords = keywords;
+    }
 }
