d1a75ca306f185a3ea1aa9f60fbef8457d02b61b
Junwei Li <lij185@cardiff.ac.uk>
2020-12-17 13:16:23 +0800

    implement query all activities

--- src/main/java/com/clientproject/soms/wellbeing/controller/ActivityController.java
+++ src/main/java/com/clientproject/soms/wellbeing/controller/ActivityController.java
@@ -3,6 +3,7 @@ package com.clientproject.soms.wellbeing.controller;
 import com.clientproject.soms.wellbeing.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,6 +16,7 @@ public class ActivityController {
         this.activityRepository = aRepo;
     }
 
+    @GetMapping("ActivityDataCaptureList")
     public ModelAndView queryAllActivity(){
 
         ModelAndView mav = new ModelAndView();
