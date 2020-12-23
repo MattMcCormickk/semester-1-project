db6c947fde1e7217c344e6301ec8301913dff99a
Junwei Li <lij185@cardiff.ac.uk>
2020-12-23 17:04:15 +0800

    Mark ReportRepositoryJDBC as Repository.

--- src/main/java/com/clientproject/soms/wellbeing/repository/ReportRepositoryJDBC.java
+++ src/main/java/com/clientproject/soms/wellbeing/repository/ReportRepositoryJDBC.java
@@ -1,5 +1,8 @@
 package com.clientproject.soms.wellbeing.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class ReportRepositoryJDBC implements ReportRepository{
 
 }
