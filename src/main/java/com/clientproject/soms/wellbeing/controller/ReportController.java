6ff92f40b83c827027484d68ef1a302868abcbc6
Junwei Li <lij185@cardiff.ac.uk>
2020-12-23 19:14:45 +0800

    When click REPORTS on header, it can redirect to AllActivity page with all activities and connect JDBC successfully.

--- src/main/java/com/clientproject/soms/wellbeing/controller/ReportController.java
+++ src/main/java/com/clientproject/soms/wellbeing/controller/ReportController.java
@@ -1,4 +1,28 @@
 package com.clientproject.soms.wellbeing.controller;
 
+import com.clientproject.soms.wellbeing.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ReportController {
+
+    private ActivityRepository activityRepository;
+
+    @Autowired
+    public ReportController(ActivityRepository aRepo) {
+        this.activityRepository = aRepo;
+    }
+
+    @RequestMapping(value = "allActivities", method = RequestMethod.GET)
+    public ModelAndView queryAllActivity(){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("allActivity",activityRepository.findAllActivity());
+        mav.setViewName("AllActivity");
+        return mav;
+    }
+
 }
