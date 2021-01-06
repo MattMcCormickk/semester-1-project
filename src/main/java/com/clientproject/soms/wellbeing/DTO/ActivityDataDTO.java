d8bce87b72c0a989488acc462e77b79e67861caf
Matthew McCormick <mccormickm1@cardiff.ac.uk>
2021-01-06 17:47:38 +0000

    deleted unused saved activities file and added search bar and radio selection to activity data capture page

--- src/main/java/com/clientproject/soms/wellbeing/DTO/ActivityDataDTO.java
+++ src/main/java/com/clientproject/soms/wellbeing/DTO/ActivityDataDTO.java
@@ -4,23 +4,21 @@ import java.util.Date;
 
 public class ActivityDataDTO {
     private int activityID;
-    private float hours;
-    private String activityName;
-    private String description;
+    private int serProID;
+    private float duration;
     private int noBagsRubbish;
     private int noVolunteer;
-    private int userID;
     private Date activityDate;
 
-    public ActivityDataDTO(int activityID, float hours, String activityName, String description, int noBagsRubbish, int noVolunteer, int userID, Date activityDate) {
+
+    public ActivityDataDTO(int activityID, int serProID, java.sql.Date activityDate, int noVolunteer, float duration, int noBagsRubbish) {
+
         this.activityID = activityID;
-        this.hours = hours;
-        this.activityName = activityName;
-        this.description = description;
+        this.duration = duration;
         this.noBagsRubbish = noBagsRubbish;
         this.noVolunteer = noVolunteer;
-        this.userID = userID;
         this.activityDate = activityDate;
+        this.serProID = serProID;
     }
 
     public int getActivityID() {
@@ -28,15 +26,7 @@ public class ActivityDataDTO {
     }
 
     public float getHours() {
-        return hours;
-    }
-
-    public String getActivityName() {
-        return activityName;
-    }
-
-    public String getDescription() {
-        return description;
+        return duration;
     }
 
     public int getNoBagsRubbish() {
@@ -45,10 +35,6 @@ public class ActivityDataDTO {
 
     public int getNoVolunteer() {
         return noVolunteer;
-    }
-
-    public int getUserID() {
-        return userID;
     }
 
     public Date getActivityDate() {
